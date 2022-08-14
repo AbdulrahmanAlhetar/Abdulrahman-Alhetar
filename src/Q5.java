a2793a8536eca0f9ebbd2449dce4cb199da1f8da
AbdulrahmanAlhetar <111249657+AbdulrahmanAlhetar@users.noreply.github.com>
2022-08-14 23:57:48 +0300

    Add files via upload

---
+++ src/Q5.java
@@ -0,0 +1,8 @@
+public class Q5 {
+    public static void main(String[] args) {
+        System.out.println("______________________");
+        System.out.println("| Abdulrahman Alhetar |");
+        System.out.println("----------------------");
+
+    }
+}
